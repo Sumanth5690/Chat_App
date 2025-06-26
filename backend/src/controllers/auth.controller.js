138d3a94ea80dfa67732b050596f4e15547d8c91
sumanth <sumanthsumanthacharya8@gmail.com>
2025-06-26 23:21:43 +0530

    initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup=(req,res)=>{
+
+}
+
+export const login=(req,res)=>{
+res.sned("auth route")
+}
+
+export const logout=(req,res)=>{
+res.sned("auth route")
+}
---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import authroute from './routes/auth.route.js'
+import dotenv from  'dotenv'
+import { connectdb } from './lib/db.js'
+
+
+const app=express()
+dotenv.config()
+
+
+const port=process.env.PORT || 5000
+
+app.use('/api/auth',authroute)
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+    connectdb()
+
+})
